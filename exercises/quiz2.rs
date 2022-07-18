aac1e6aab7df9ff2274576c0ecd7e22c99d303a3
srosati <srosati@itba.edu.ar>
2022-07-18 12:09:16 -0300

    quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -29,12 +27,14 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            output.push(match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(times) => string.to_string() + &"bar".repeat(*times)
+            });
         }
         output
     }
@@ -43,7 +43,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What to we have to import to have `transformer` in scope?
-    use ???;
+    use my_module::transformer;
     use super::Command;
 
     #[test]
--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -1,8 +1,6 @@
 // strings1.rs
 // Make me compile without changing the function signature!
-// Execute `rustlings hint strings1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// Execute `rustlings hint strings1` or use the `hint` watch subcommand for a hint.s
 
 fn main() {
     let answer = current_favorite_color();
@@ -10,5 +8,5 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    String::from("blue")
 }
